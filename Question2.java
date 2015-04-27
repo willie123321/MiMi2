464a53816eec5414698ed5290bfe0104d1b95c48
willie123321 <willie820316@hotmail.com>
2015-04-27 10:51:32 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -2,6 +2,6 @@
 	public static void main(String[] args){
 		txtCopy("Source.txt", "Dest.txt");
 		editFile("Dest.txt");
-		imgCopy("Source.jpg", "Dest.jpg");
+		imgCopy("Sourcce.jpg", "Dest.jpg");
 	}
 }
